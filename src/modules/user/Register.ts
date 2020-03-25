599b598039c4767eb632587861e6bdec2fa4de7a
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2020-03-25 13:30:00 +0100

    delete middleware from hello query

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -1,15 +1,14 @@
-import * as bcrypt from 'bcryptjs';
-import { Arg, Mutation, Query, Resolver, UseMiddleware } from "type-graphql";
-import { User } from '../../entity/User';
-import { withAuthenticatedUser } from '../middleware/withAuthenticatedUser';
-import { createAccountConfirmationUrl } from '../utils/createAccountConfirmationUrl';
-import { sendEmail } from '../utils/sendEmail';
-import { RegisterInput } from './register/RegisterInput';
+import * as bcrypt from "bcryptjs";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../../entity/User";
+// import { withAuthenticatedUser } from '../middleware/withAuthenticatedUser';, UseMiddleware
+import { createAccountConfirmationUrl } from "../utils/createAccountConfirmationUrl";
+import { sendEmail } from "../utils/sendEmail";
+import { RegisterInput } from "./register/RegisterInput";
 
 @Resolver(User)
 export class RegisterResolver {
-
-  @UseMiddleware(withAuthenticatedUser)
+  // @UseMiddleware(withAuthenticatedUser)
   @Query(() => String)
   hello() {
     return "hello!";
@@ -17,19 +16,19 @@ export class RegisterResolver {
 
   @Mutation(() => User)
   async register(
-    @Arg("data") {email, password} :RegisterInput,
-  ):Promise<User> {
-      const hashedPassword = await bcrypt.hash(password, 12);
-      const user = await User.create({
-        email,
-        password: hashedPassword
-      }).save();
+    @Arg("data") { email, password }: RegisterInput
+  ): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.create({
+      email,
+      password: hashedPassword
+    }).save();
 
-      await sendEmail({
-        email,
-        url: await createAccountConfirmationUrl(user.id)
-      })
+    await sendEmail({
+      email,
+      url: await createAccountConfirmationUrl(user.id)
+    });
 
-      return user;
+    return user;
   }
 }
