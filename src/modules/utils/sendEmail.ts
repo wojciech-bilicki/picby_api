ea83ac2859dd99e0b4d086680883328a6693df34
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2020-03-26 11:07:34 +0100

    change confirmation email link

--- src/modules/utils/sendEmail.ts
+++ src/modules/utils/sendEmail.ts
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import { appPassword } from "./picbyEmailPassword";
+import { appPassword } from "./picbyEmailPass";
 
 interface SendEmailArgs {
   email: string;
