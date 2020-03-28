e233389bf4ae7e8f263d5598a8d10258d9bf705a
Marcin Jarowski <jarowski.marcin@yahoo.co.uk>
2020-03-28 19:02:08 +0100

    forgot password- return true or false depending if user is found

--- src/modules/user/ChangePassword.ts
+++ src/modules/user/ChangePassword.ts
@@ -1,29 +1,26 @@
-import bcrypt from 'bcryptjs';
-import { Context } from 'src/types/Context';
-import { Arg, Ctx, Mutation, Resolver } from 'type-graphql';
-import { User } from '../../entity/User';
-import { redis } from '../../redis';
-import { FORGET_PASSWORD_PREFIX } from '../constants/redisPrefixes';
-import { ChangePasswordInput } from './changePassword/ChangePasswordInput';
-
-
+import bcrypt from "bcryptjs";
+import { Context } from "src/types/Context";
+import { Arg, Ctx, Mutation, Resolver } from "type-graphql";
+import { User } from "../../entity/User";
+import { redis } from "../../redis";
+import { FORGET_PASSWORD_PREFIX } from "../constants/redisPrefixes";
+import { ChangePasswordInput } from "./changePassword/ChangePasswordInput";
 
 @Resolver(User)
 export class ChangePasswordResolver {
-  @Mutation(() => User, {nullable: true})
+  @Mutation(() => User, { nullable: true })
   async changePassword(
-    @Arg('data') {token, password}: ChangePasswordInput,
+    @Arg("data") { token, password }: ChangePasswordInput,
     @Ctx() ctx: Context
   ): Promise<User | null> {
-    
     const userId = await redis.get(FORGET_PASSWORD_PREFIX + token);
-    if(!userId) {
+    if (!userId) {
       return null;
     }
 
-    const user = await  User.findOne(userId);
+    const user = await User.findOne(userId);
 
-    if(!user) {
+    if (!user) {
       return null;
     }
 
