71e77ff86ceb67d4f17c2df0ef51a7e4d86bd2d1
Marcin Jarowski <47415939+MarcinJarowski@users.noreply.github.com>
2020-03-31 20:17:11 +0200

    fix compiling error (#6)
    * add sendRegister link email service
    
    * change port
    
    * login user - throw error codes
    
    * delete middleware from hello query
    
    * configure orm config for development needs
    
    * fixed untracked files
    
    * update gitignore, delete password from project
    
    * change confirmation email link
    
    * schema- add logout resolver
    
    * register - remove commented code
    
    * forgot password- return true or false depending if user is found
    
    * send email - change email confirm/forgot pass  message depending on passed index
    
    * fix bundle error
    
    * Fix the build in CircleCI
    
    Co-authored-by: wojtek <wojciech.bilicki@gmail.com>

--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -5,9 +5,9 @@ import { ChangePasswordResolver } from "../modules/user/ChangePassword";
 import { ConfirmUserResolver } from "../modules/user/ConfirmUser";
 import { ForgotPasswordResolver } from "../modules/user/ForgotPassword";
 import { LoginResolver } from "../modules/user/Login";
+import { LogoutResolver } from "../modules/user/Logout";
 import { MeResolver } from "../modules/user/Me";
 import { RegisterResolver } from "../modules/user/Register";
-import { LogoutResolver } from "../modules/user/LogOut";
 
 export const createSchema = async () =>
   await buildSchema({
