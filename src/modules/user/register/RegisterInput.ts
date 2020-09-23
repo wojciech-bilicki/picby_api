fc0d7aadb72a11ca7b7eb67f4bf831eff93fa8f2
wojtek <wojciech.bilicki@gmail.com>
2020-09-23 20:34:10 +0200

    Resolve #17. Fix registration error handling

--- src/modules/user/register/RegisterInput.ts
+++ src/modules/user/register/RegisterInput.ts
@@ -1,4 +1,3 @@
-import { IsEmail } from "class-validator";
 import { Field, InputType } from "type-graphql";
 import { PasswordInput } from "../../shared/PasswordInput";
 import { DoesEmailExist } from "./isEmailAlreadyExists";
@@ -6,7 +5,6 @@ import { DoesEmailExist } from "./isEmailAlreadyExists";
 @InputType()
 export class RegisterInput extends PasswordInput {
   @Field()
-  @IsEmail()
   @DoesEmailExist({message: "email already in use"})
   email!: string;
 
